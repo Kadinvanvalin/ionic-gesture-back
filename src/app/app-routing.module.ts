278a07baff33a16fe6328ea220fb6b95c7c0c2cf
kadinvanvalin <kadin@Kadins-MBP.attlocal.net>
2019-06-04 15:05:01 -0500

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'other', loadChildren: './other/other.module#OtherPageModule' },
 ];
 
 @NgModule({
